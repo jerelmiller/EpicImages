71634918f855c3b35c207e8227fa0de21349f0d6
Jerel Miller <jmiller@24tru.com>
2013-03-26 23:57:49 -0600

    setting initalize_on_precompile to false

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module EpicImages
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't bootstrap application assets
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
