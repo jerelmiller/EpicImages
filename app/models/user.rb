c95993dde422f5912f1cc7964b8c8e6213b945a0
Jerel Miller <jermille@mines.edu>
2013-04-22 01:32:19 -0600

    Some styling on the admin section

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, confirmation: true, on: :update, unless: lambda { self.password.blank? }
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
+
+  def to_s
+    "#{first_name} #{last_initial}"
+  end
+
+  private
+
+    def last_initial
+      "#{last_name.split('').first}."
+    end
 end
