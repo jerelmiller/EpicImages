0e34285e929123a5cac249bf241a2c547005fdb3
Jerel Miller <jmiller@24tru.com>
2013-04-25 00:52:34 -0600

    Admin dropdown works correctly. Some styling to form buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,17 @@ module ApplicationHelper
     time.strftime '%A, %B %-d, %Y'
   end
 
-  def submit_text(model)
-    return "Create #{model}" if params[:action].eql? 'new'
-    "Update #{model}"
+  def save_button
+    content_tag :button, class: 'button' do
+      concat content_tag :span, '', class: 'glyph save'
+      concat 'Save'
+    end
+  end
+
+  def link_to_cancel(path)
+    link_to path, class: 'button red' do
+      concat content_tag :span, '', class: 'icon16 cross'
+      concat 'Cancel'
+    end
   end
 end
